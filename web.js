cd11161001f6e360de7d0b128a28e36f07ec2732
Gaba <gabelula@gmail.com>
2014-04-15 19:03:21 -0300

    Change some info and add web.js

---
+++ web.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var app = express();
+app.use(express.logger());
+app.use("/", express.static(__dirname));
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+    console.log("Listening on " + port);
+});
